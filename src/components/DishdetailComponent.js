01461098a74044c58acc00117c257d14c436d012
psadeck76 <psadeck@gmail.com>
2020-10-27 23:00:56 -0400

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import moment from 'moment'
 
-    function RenderDish({dish}){  
-      //console.log(dish)    
-      //console.log({dish})    
-      if (dish != null){          
-        return(          
+    function RenderDish({dish}){
+      if (dish != null){
+        return(
             <Card>
-              <CardImg  width="100%" src={dish.image} alt={dish.name} />             
-              <CardBody>            
-                  <CardTitle>{dish.name}</CardTitle>                      
-                  <CardText>{dish.description}</CardText>                  
-                  <CardText>{dish.comments[0].comment}</CardText>
-              </CardBody>                     
-            </Card>               
+              <CardImg  width="100%" src={dish.image} alt={dish.name} />
+              <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
         );
       }
       else{
         return(
           <div>
-          
+
           </div>
         );
       }
     }
 
-    function RenderComments({comments}){      
-      if (comments != null){           
-          const comm = comments.map((comment) =>{ 
+    function RenderComments({comments}){
+      if (comments != null){
+          const comm = comments.map((comment) =>{
               return(
-              <div>              
+              <div>
                 <ul style={{listStyleType: 'none'}}>
                   <li>
                     {comment.comment}
@@ -38,10 +35,10 @@ import moment from 'moment'
                   <br/>
                   <li>
                       -- {comment.author}, {moment(comment.date).format('MMMM DD YYYY')}
-                  </li>                 
+                  </li>
                 </ul>
-              </div>    
-              );        
+              </div>
+              );
       })
           return(
             <div>
@@ -57,29 +54,28 @@ import moment from 'moment'
       else{
         return(
         <div>
-          No Comments                  
+          No Comments
         </div>
         );
-      };      
+      };
     }
 
-      const  DishDetail = (props) => {      
+      const  DishDetail = (props) => {
         console.log(props.dish)
-            // Why is props.dish null???
-            if(props.dish !=null){            
-              return ( 
+            if(props.dish !=null){
+              return (
                 <div className="container">
                   <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                      <RenderDish dish={props.dish} />                
-                    </div> 
+                      <RenderDish dish={props.dish} />
+                    </div>
                     <div className="col-12 col-md-5 m-1">
                       <RenderComments comments={props.dish.comments} />
-                    </div>          
-                  </div> 
+                    </div>
+                  </div>
                 </div>
               )
-            }//End if
+            }
             else{
               return(
                 <div>
